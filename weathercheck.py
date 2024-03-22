dbc81ab8a20ce5a9b007e075179519e52fea5ae4
Shivkansh Singh <161964686+shivkansh-singh@users.noreply.github.com>
2024-03-22 14:43:54 +0530

    Update weathercheck.py

--- weathercheck.py
+++ weathercheck.py
@@ -3,7 +3,7 @@ import sys
 
 zipcode = input("Enter the zip code: ")
 countrycode = input("Enter your country code: ")
-apikey="1eb0d3498a7333f8a01072325a17d3d0"
+#apikey="1eb0d3498a7333f8a01072325a17d3d"
 #6b970fab4b35817499b86ddcf0b2b437
 weather_url = f"https://api.openweathermap.org/data/2.5/weather?zip={zipcode},{countrycode}&appid={apikey}"
 
@@ -15,3 +15,4 @@ if result.status_code !=200:
 else:
   print(result.json())
   sys.exit(0)
+#0
